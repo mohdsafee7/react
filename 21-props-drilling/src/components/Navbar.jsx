83ecf00461f645c88b9e5179b5e673494ed2c5e6
Mohd Safee <2k23.cs2313689@gmail.com>
2026-02-16 20:24:29 +0530

    props drilling

---
+++ 21-props-drilling/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = (props) => {
+  console.log(props);
+  
+  return (
+    <div>
+      <button onClick={()=>{
+        props.setTheme('Dark')
+      }}>Change Theme</button>
+    </div>
+  )
+}
+
+export default Navbar
